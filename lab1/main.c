4f9dedb728afdd0ef3231296b1a8d2cfaa321f83
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-03-16 13:59:22 +0300

    Add files via upload

--- lab1/main.c
+++ lab1/main.c
@@ -4,28 +4,47 @@
 int main(void)
 {
     char number[100];
-    
+    int res;
     printf("enter real number\n");
-    scanf("%99s", number);
-    printf("MAX sum: %d", Sum(number));
+    
+    do
+    {
+        scanf("%99s", number);
+        res = Sum(number);
+    } while (res < 0);
+    
+    printf("MAX sum: %d", res);
 }
 
 int Sum(char *str)
 {
     int sum1 = 0, sum2 = 0, sum = 0, i = 0;
 
-    while (str[i] != '.')
+    while (str[i] != '\0' && str[i] != '.')
     {
+        if(str[i] < '0' || str[i] > '9')
+        {
+            printf("invalid input");
+            return -1;
+        }
         sum1 += str[i] - '0';
         i++;
     }
+
     i++;
+    
     while (str[i])
     {
+        if(str[i] < '0' || str[i] > '9')
+        {
+            printf("invalid input");
+            return -1;
+        }
         sum2 += str[i] - '0';
         i++;
     }
 
     sum = (sum1 <= sum2) ? sum2 : sum1;
+
     return sum;
 }
--- lab1/main.test.c
+++ lab1/main.test.c
@@ -7,9 +7,16 @@ void test_case1(void)
     assert(r == 14);
 }
 
+void test_case2(void)
+{
+    int r = Sum("45.cs7");
+    assert(r == -1);
+}
+
 #undef main
 
 int main(void)
 {
     test_case1();
+    test_case2();
 }
