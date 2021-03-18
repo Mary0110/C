55c2213d6acb35fd57a0808392c631ce6f97b2a7
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-03-18 10:30:28 +0300

    Add files via upload

---
+++ lab2/2.1/main.test.c
@@ -0,0 +1,40 @@
+#include <assert.h>
+#include "main.h"
+
+void test_area(void)
+{
+    float a = area(2);
+    assert(a == 12.56);
+}
+void test_sector(void)
+{
+    float b = sector(3, 4);
+    assert(b == 18);
+}
+void test_circumf(void)
+{
+    float c = circumf(7);
+    assert(c == 43.96);
+}
+void test_square(void)
+{
+    float d = square(5.1);
+    assert(d == 7.21);
+}
+void test_cone(void)
+{
+    float e = cone(7.9);
+    assert(e == 516.04);
+}
+
+#undef main
+
+int main(void)
+{
+    void test_area();
+    void test_sector();
+    void test_circumf();
+    void test_square();
+    void test_cone();
+    void test_cone();
+}
---
+++ lab2/2.2/main.h
@@ -0,0 +1,7 @@
+unsigned int fact(int x);
+int sinFirst(double angle, double e, int n);
+int sinSecond(double angle, double e, int n);
+double subFunct(double angle, int n); 
+
+
+
