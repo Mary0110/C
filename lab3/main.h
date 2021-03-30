1aceb2f5971b5babb468ef302952eb44efe4e6dd
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-03-30 22:44:12 +0300

    Delete lab3 directory

--- lab3/main.h
+++
@@ -1,7 +0,0 @@
-
-void fill(int** field);
-void printField(int** field);
-void place(int** field);
-void surround(int a, int b, int f, int size, int** field);
-int checkShips(int** field);
-void clear(int** field);
