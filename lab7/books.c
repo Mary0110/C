96c4ae5db1443ebcec13091f11b69a67420ad80d
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-06-01 22:09:50 +0300

    Update books.c

--- lab7/books.c
+++ lab7/books.c
@@ -3,6 +3,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+ void reverse(char* s)
+ {
+     int i, j;
+     char c;
+ 
+     for (i = 0, j = strlen(s)-1; i<j; i++, j--) 
+     {
+         c = s[i];
+         s[i] = s[j];
+         s[j] = c;
+     }
+ }
+
+char* itoa(int n, char s[])
+{
+     int i, sign;
+ 
+     if ((sign = n) < 0)  /* записываем знак */
+         n = -n;          /* делаем n положительным числом */
+     i = 0;
+     do {       /* генерируем цифры в обратном порядке */
+         s[i++] = n % 10 + '0';   /* берем следующую цифру */
+     } while ((n /= 10) > 0);     /* удаляем */
+     if (sign < 0)
+         s[i++] = '-';
+     s[i] = '\0';
+     reverse(s);
+    return s;
+}
+
 int addBook (bookList_t* lib, bookNode_t* book)
 {        
     book->next = lib->head;
@@ -134,11 +164,11 @@ int print_list(bookList_t* list, FILE* file)
         sprintf(price, "%f", book.price);
         fputs(price, file); 
         fputs(enumStringsst[book.status], file);
-        itoa(book.returnDate.day, price, 2);
+        itoa(book.returnDate.day, price);
         fputs(price, file); 
-        itoa(book.returnDate.month, price, 2); 
+        itoa(book.returnDate.month, price); 
         fputs(price, file);
-        itoa(book.returnDate.year, price, 2);
+        itoa(book.returnDate.year, price);
         fputs(price, file);
        
         fputs(book.remarks, file);
