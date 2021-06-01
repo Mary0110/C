9b05af0dabf5108a76fe23d7695d22d80fbedd0a
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-06-01 21:36:43 +0300

    Add files via upload

---
+++ lab7/books.c
@@ -0,0 +1,159 @@
+
+#include "books.h"
+#include <stdlib.h>
+#include <string.h>
+
+int addBook (bookList_t* lib, bookNode_t* book)
+{        
+    book->next = lib->head;
+    book->prev = NULL;
+    if(lib->head)
+    {
+        lib->head->prev = book;
+    }
+    lib->head = book;
+    return 0;
+}
+
+int deleteBook(bookList_t** lib, bookNode_t* del)
+{
+    if(*lib == NULL || del == NULL)
+        return 0;
+
+    if((*lib)->head == del)
+        (*lib)->head = del->next;
+
+    if (del->next != NULL)
+       del->next->prev = del->prev;
+
+    if (del->prev != NULL)
+        del->prev->next = del->next;
+
+    free(del);
+    return 0;
+}
+
+int count(bookList_t* list)
+{
+    int count = 0;
+    bookNode_t* temp = list->head;
+    while(temp)
+    {        
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+float overallPrice(bookList_t* list)
+{
+    float count = 0;
+    bookNode_t* temp = list->head;
+    while(temp)
+    {
+        count += temp->book.price;
+        temp = temp->next;
+    }
+    return count;
+}
+
+
+int countBorrowed(bookList_t* list)
+{
+    int count = 0;
+    bookNode_t* temp = list->head;
+    while(temp)
+    {  
+        if(temp->book.status == 0)      
+            count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+bookNode_t* searchBook(bookList_t* List, const char *name)
+{
+    bookNode_t* temp = List->head;
+    while(temp)
+    {
+        if(strcmp(temp->book.bookName, name) == 0)
+            return temp;
+        temp = temp->next;
+    }
+    return NULL;
+}
+
+bookList_t* readBookList(const char* filename)
+{
+    FILE* file;
+    char price[20]; char st[20];
+    bookList_t *temp = (bookList_t*) malloc(sizeof(bookList_t));
+    if (temp == NULL)
+        exit(1);
+    temp->size = 0;
+    temp->head = temp->tail = NULL;
+     file =fopen( filename, "r");
+    if (!file)
+        exit(1);
+    while(!feof(file))
+    {
+        bookNode_t* book = (bookNode_t*)malloc(sizeof(bookNode_t*));
+        if (!book)
+            exit(1);
+        fgets(book->book.bookName, 19, file);
+        fgets(book->book.author, 19, file);
+        
+        fgets( price, 19, file);
+        book->book.price = atof(price);
+        fgets(st, 19, file);
+        if (strcmp(st, "inStock"))
+            book->book.status = 0;
+        else
+            book->book.status = 1;
+        fgets(book->book.remarks, 19, file);
+        fgets(price, 2, file);book->book.returnDate.day = atoi(price);
+        fgets(price, 2, file);book->book.returnDate.month = atoi(price);
+        fgets(price, 2, file);book->book.returnDate.year = atoi(price);
+        addBook(temp, book);
+    }
+    printf("read");
+    fclose(file);
+    return temp;
+}
+
+int print_list(bookList_t* list, FILE* file) 
+{
+    char price[20] ;
+    bookNode_t* node = list->head;
+    size_t count;
+    for (count = 0; count < list->size; ++count) 
+    {
+        book_t book = node->book;
+        fputs(book.bookName, file);
+        fputs(book.author, file);
+        sprintf(price, "%f", book.price);
+        fputs(price, file); 
+        fputs(enumStringsst[book.status], file);
+        itoa(book.returnDate.day, price, 2);
+        fputs(price, file); 
+        itoa(book.returnDate.month, price, 2); 
+        fputs(price, file);
+        itoa(book.returnDate.year, price, 2);
+        fputs(price, file);
+       
+        fputs(book.remarks, file);
+
+        if (count < list->size - 1) 
+        {
+            fputs("\t", file);
+        }
+        node = node->next;
+    }
+    return 0;
+}
+
+
+
+
+
+
---
+++ lab7/main.c
@@ -0,0 +1,366 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "readers.h"
+#include<string.h>
+
+int searchReaderInput(readerNode_t* current, readerList_t* readers);
+int searchBookInput(bookNode_t* current, bookList_t* lib);
+char* receiveInput(char* s);
+
+int main(void)
+{
+    int endApp = 0;
+    int opt; float num; int f = 0, o = 0; unsigned int ush, day, month, year;
+    char str[20];
+    
+    FILE* outputFile = NULL, *outputFile2 = NULL;
+    bookList_t* library;
+    readerList_t* readers; 
+    printf("x;jnj");
+    library = readBookList("lib.txt");
+     readers = readReaderList("readers.txt");
+    while (!endApp)
+    {
+        printf("LIBRARY");
+        printf("Choose option:\n");
+        printf("\t 1 - buy new book \n");
+        printf("\t 2 - buy a copy of a book \n");
+        printf("\t 3 - add new reader\n");
+        printf("\t 4 - edit information \n");
+        printf("\t 5 - delete \n ");
+        printf("\t 6 - give out book\n");
+        printf("\t 7 - return book\n");
+        printf("\t 8 - fine reader\n");
+        printf("\t 9 - report\n");
+        printf("\t 10 - exit\n");
+
+        do
+        {
+            o = scanf("%d", &opt);
+            getchar();
+        } while (!(o > 0 && o <= 10));
+
+        switch (opt)
+        {
+        case 1:
+        {
+            bookNode_t* newBook = (bookNode_t*)malloc(sizeof(bookNode_t));
+            if (newBook != NULL)
+            {
+                int f = 0; char str[20];
+                float num;
+                printf("Enter name of new book: \n");
+               
+                strcpy(newBook->book.bookName,  receiveInput(str));
+                printf("Enter author of new book: \n");
+                strcpy(newBook->book.author, receiveInput(str));
+                while (!f)
+                {
+                    char c[20];
+                    printf("Enter price of new book: \n");
+                    fgets(c, 19, stdin);
+                    num = atof(c);
+                    if (num > 0)
+                    {
+                        newBook->book.price = num;
+                        f = 1;
+                    }
+                    else
+                    {
+                        printf("Invalid input");
+                    }
+                }
+                printf("Add remarks to new book: \n");
+                strcpy(newBook->book.remarks, receiveInput(str));
+                newBook->book.status = inStock;
+                newBook->book.returnDate.day = 0;
+                newBook->book.returnDate.month = 0;
+                newBook->book.returnDate.year = 0;
+                addBook(library, newBook);
+            }
+        }
+        break;
+        case 2:
+        {
+            bookNode_t* newBook, *b;
+            char str[20];
+            printf("Enter name of book to copy:\t");
+            newBook = (bookNode_t*)malloc(sizeof(bookNode_t));
+            b = searchBook(library, receiveInput(str));
+            if (newBook && b)
+            {
+                strcpy(newBook->book.bookName,  b->book.bookName);
+                 strcpy(newBook->book.author , b->book.author);
+                newBook->book.price = b->book.price;
+                newBook->book.status = inStock;
+                newBook->book.returnDate.day = 0;
+                newBook->book.returnDate.month = 0;
+                newBook->book.returnDate.year = 0;
+                addBook(library, newBook);
+            }
+            else
+                printf("lib is empty");
+        }
+        break;
+        case 3:
+        {
+            bookList_t* library;
+            readerNode_t* newReader = (readerNode_t*)malloc(sizeof(readerNode_t));
+            int f = 0; char str[20];
+            float num;
+            printf("Enter name of new reader: \n");
+            strcpy(newReader->reader.name, receiveInput(str)); 
+            printf("Enter surname: \n");
+            strcpy(newReader->reader.surname, receiveInput(str));
+           
+            do
+            {
+                printf("Enter group number: \n");
+            } while (!scanf("%u", &newReader->reader.groupNumber));
+            do
+            {
+                printf("Enter phone number: \n");
+            } while (!scanf("%u", &newReader->reader.phoneNumber));
+            
+            printf("Enter email of new reader: \n");
+            strcpy(newReader->reader.email, receiveInput(str));
+          
+            newReader->reader.card =0;
+          
+            printf("Add remarks: \n");
+            strcpy(newReader->reader.remarks, receiveInput(str));
+            
+             library = (bookList_t*)malloc(sizeof(bookList_t));
+            newReader->reader.borrowedList = *library;
+            addReader(readers, newReader);
+        }
+        break;
+        case 4:
+        {
+            printf("Choose 1 for editing book info, 2 for editing reader info");
+            while (!(scanf("%d", &opt) && (opt == 1 || opt == 2)))
+            {
+                printf("Invalid input");
+            }
+            switch (opt)
+            {
+            case(1):
+            {
+                int f = 0; char str[20];
+                float num;
+                bookNode_t* newBook = NULL;
+                searchBookInput(newBook, library);
+                
+                if (newBook == NULL)
+                    exit(1);
+
+                
+                printf("Enter name of new book: \n");
+                strcpy(newBook->book.bookName, receiveInput(str));
+               
+                printf("Enter author of new book: \n");
+                strcpy(newBook->book.author, receiveInput(str));
+               
+                while (!f)
+                {
+                    char c[20];
+                    printf("Enter price of new book: \n");
+                    fgets(c, 19, stdin);
+                    num = atof(c);
+                    if (num > 0)
+                    {
+                        newBook->book.price = num;
+                        f = 1;
+                    }
+                    else
+                    {
+                        printf("Invalid input");
+                    }
+                }
+                printf("Add remarks to new book: \n");
+                strcpy(newBook->book.remarks, receiveInput(str));
+                newBook->book.status = inStock;
+                newBook->book.returnDate.day = 0;
+                newBook->book.returnDate.month = 0;
+                newBook->book.returnDate.year = 0;
+
+            }
+            break;
+            case(2):
+            {
+                int f = 0; char str[20];
+                float num;
+                readerNode_t* newReader = NULL;bookList_t* library;
+                searchReaderInput(newReader, readers);
+                
+                
+                printf("Enter name of new reader: \n");
+                strcpy(newReader->reader.name, receiveInput(str));
+                
+                printf("Enter surname: \n");
+                strcpy(newReader->reader.surname, receiveInput(str));
+            
+                do
+                {
+                    printf("Enter group number: \n");
+                } while (!scanf("%u", &newReader->reader.groupNumber));
+                do
+                {
+                    printf("Enter phone number: \n");
+                } while (!scanf("%u", &newReader->reader.phoneNumber));
+
+                printf("Enter email of new reader: \n");
+                strcpy(newReader->reader.email, receiveInput(str));
+         
+                newReader->reader.card = 0;
+
+                printf("Add remarks: \n");
+                strcpy(newReader->reader.remarks, receiveInput(str));
+             
+                library = (bookList_t*)malloc(sizeof(bookList_t));
+                newReader->reader.borrowedList = *library;
+            }
+            break;
+            }
+        }
+        break;
+        case 5:
+        {
+            printf("Choose 1 for removing book, 2 for removing reader");
+            while (!(scanf("%d", &opt) && (opt == 1 || opt == 2)))
+            {
+                printf("Invalid input");
+            }
+            switch (opt)
+            {
+            case(1):
+            {
+                bookNode_t* current = NULL;
+                searchBookInput(current, library);
+                deleteBook(&library, current);
+            }
+            break;
+            case(2):
+            {
+                readerNode_t* current = NULL;
+                searchReaderInput(current, readers);
+                deleteReader(&readers, current);
+            }
+            break;
+            }
+        }
+        break;
+        case 6:
+        {
+            readerNode_t* current = NULL; bookNode_t* b = NULL;
+            searchReaderInput(current, readers);
+            
+            searchBookInput(b, library);
+            do
+            {
+                printf("enter day:");
+
+            } while (!(scanf("%u", &day) && day <= 31));
+            do
+            {
+                printf("enter month:");
+
+            } while (!(scanf("%u", &month) && month <= 12));
+            do
+            {
+                printf("enter year:");
+
+            } while (!(scanf("%u", &year) && year >= 2021));
+            giveOutBook(current->reader, b, day, month, year);
+        }
+        break;
+        case 7:
+        {
+            readerNode_t* current = NULL;bookNode_t* b = NULL;
+            searchReaderInput(current, readers);
+            
+            searchBookInput(b, library);
+            returnBook(&current->reader, b);
+            printf("book is in stock");
+        }
+        break;
+        case 8:
+        {
+            readerNode_t* current = NULL;
+            searchReaderInput(current, readers);
+            printf("choose card: 1 - yellow, 2 - red");
+            do
+            {
+                scanf("%u", &opt);
+            } while (!(opt == 1 || opt == 2));
+
+            fineReader(&current->reader, opt);
+        }
+        break;
+        case 9:
+        {
+            /*The number of books in library:*/
+            printf(" %d\n", count(library));
+            /*The number of borrowed books in library*/
+            printf("The number of borrowed books in library: %d\n", countBorrowed(library));
+            /*"Overall price*/
+            printf("Overall price: %f\n", overallPrice(library));
+            /*The number of readers in library*/
+            printf(": %d\n", countReaders(readers));
+        }
+        break;
+        case 10:
+        default:
+            endApp = 1;
+            break;
+        }
+    }
+        fopen_s(&outputFile, "output.txt", "w");
+        if (outputFile == NULL)
+            exit(1);
+        print_readers(readers, outputFile);
+         fopen_s(&outputFile2, "output2.txt", "w");
+        if (outputFile2 == NULL)
+            exit(1);
+        print_list(library, outputFile2);
+
+        printf("-----------------------\n");
+        return 0;
+}
+int searchReaderInput(readerNode_t* current, readerList_t* readers)
+{
+    unsigned short ush;
+    printf("enter the phone number of a reader");
+    while (!scanf("%u", &ush))
+    {
+        printf("invalit input. Try again");
+    }
+    current = searchReader(readers, ush);
+    while (current == NULL)
+    {
+        printf("Invalid input. Enter phone number of a person: ");
+        while (!scanf("%u", &ush))
+        {
+            printf("invalit input. Try again");
+        }
+        current = searchReader(readers, ush);
+    }
+    return 0;
+}
+int searchBookInput(bookNode_t* current, bookList_t* lib)
+{
+    char str[20];
+    printf("Enter name of a book: ");
+    current = searchBook(lib, receiveInput(str));
+    while (current == NULL)
+    {
+        printf("Invalid input. Enter name of a book again: ");
+        current = searchBook(lib, receiveInput(str));
+    }
+    return 0;
+}
+char* receiveInput(char* s)
+{
+    fgets(s, 19,stdin);
+    return s;
+}
