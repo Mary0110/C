1c59c46fb71b5088626cd503f403c1dcf3fecebf
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-06-03 12:08:36 +0300

    Update books.c

--- lab7pr/books.c
+++ lab7pr/books.c
@@ -3,19 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
- void reverse(char* s)
- {
-     int i, j;
-     char c;
- 
-     for (i = 0, j = strlen(s)-1; i<j; i++, j--) 
-     {
-         c = s[i];
-         s[i] = s[j];
-         s[j] = c;
-     }
- }
-
 int addBook (bookList_t* readerList, book_t* book)
 {     
         bookNode_t* node = (bookNode_t*)malloc(sizeof(bookNode_t));
