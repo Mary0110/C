4bc0eba53e0966b839ac341eae057637b894f517
Maria Raniuk <69244827+Mary0110@users.noreply.github.com>
2021-06-03 11:51:30 +0300

    Update test7.c

--- lab7pr/test7.c
+++ lab7pr/test7.c
@@ -49,7 +49,6 @@ int main(void)
 	assert(returnBook(list->head->reader, blist->head) == 0);
 	assert(fineReader(list->head->reader, 1) == 0);
 	assert(countReaders(list) == 3);
-	printf("%d%d",count(blist),countBorrowed(blist));
 	assert(count(blist) == 6);
 	assert(countBorrowed(blist) == 5);
 	printf("tested successfully");
